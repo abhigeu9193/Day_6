f899e3b9364a1ed499ce8bf18df72cc09bad16c2
Abhishek <abhigeu9193@gmail.com>
2022-04-07 15:59:21 +0530

    [Abhishek]problem_6

---
+++ problem6.java
@@ -0,0 +1,19 @@
+public class problem6 {
+        public static void primeFactors(int n)
+        {
+            int c = 2;
+            while (n > 1) {
+                if (n % c == 0) {
+                    System.out.print(c + " ");
+                    n /= c;
+                }
+                else
+                    c++;
+            }
+        }
+        public static void main(String[] args)
+        {
+            int n = 9;
+            primeFactors(n);
+        }
+    }
