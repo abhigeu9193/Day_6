acd14d00a357f7230b00a7e718e273f40f98cd8f
Abhishek <abhigeu9193@gmail.com>
2022-04-07 11:02:49 +0530

    Initial commit

---
+++ problem2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+ 
+class problem2 {
+    static double sum(int n)
+    {
+      double i, s = 0.0;
+      for (i = 1; i <= n; i++)
+          s = s + 1/i;
+      return s;
+    }
+    public static void main(String args[])
+    {
+        int n = 5;
+        System.out.printf("Sum is %f", sum(n));
+         
+    }
+}
